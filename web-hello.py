4708679f7b5d861ed73c01a8d1e5ffb72d740407
Siddharth Thangharaj <tsiddharth46@gmail.com>
2017-03-23 18:51:27 -0300

    Adds first python web app

---
+++ web-hello.py
@@ -0,0 +1,18 @@
+from flask import Flask
+rt Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello World!"
+
+    if __name__ == "__main__":
+            app.run()
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
